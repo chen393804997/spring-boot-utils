8828831795a64268780f14e646a104b0d869591b
czw <chenzhiwu>
2019-09-24 14:53:13 +0800

    初始化

---
+++ src/main/java/com/example/springbootutils/SpringBootUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUtilsApplication.class, args);
+    }
+
+}
